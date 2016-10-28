975a1cb27a493786ba5900961b6a1c1c6c226ae2
David Alexander <dalexander@pacificbiosciences.com>
2016-10-28 15:42:23 -0700

    Deploy python stack to real VE location

--- deploy/deployPythonStack.sh
+++ deploy/deployPythonStack.sh
@@ -10,14 +10,11 @@ module load gcc/4.8.4 python/2.7.9 zlib/1.2.5 openblas/0.2.14
 set -euo pipefail
 set -x
 
-#BAUHAUS_VE=/mnt/secondary/Share/MLG/Bauhaus/VE-Bauhaus
-BAUHAUS_VE=/tmp/VE-Bauhaus
-
-
+BAUHAUS_VE=/mnt/secondary/Share/MLG/Bauhaus/VE-Bauhaus
 rm -rf $BAUHAUS_VE
 
 set +u
-/mnt/software/v/virtualenv/13.0.1/virtualenv.py  $BAUHAUS_VE
+/mnt/software/v/virtualenv/13.0.1/virtualenv.py $BAUHAUS_VE
 source $BAUHAUS_VE/bin/activate
 set -u
 
